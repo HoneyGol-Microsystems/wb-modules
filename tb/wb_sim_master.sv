a220c6351d65ba09585195583a3a8b608b997032
Ondrej Golasowski <golasowski.o@gmail.com>
2024-10-25 19:58:38 +0200

    Fixed assignment types, signal names.

--- tb/wb_sim_master.sv
+++ tb/wb_sim_master.sv
@@ -15,20 +15,20 @@ class wb_sim_master #(
         input  logic [(DATA_WIDTH/8)-1:0]   mask,
         output logic [DATA_WIDTH-1:0]       read_data
     );
-        this.wb_if.adr = addr;
-        this.wb_if.we  = 1'b0;
-        this.wb_if.sel = mask;
-        this.wb_if.stb = 1'b1;
-        this.wb_if.cyc = 1'b1;
+        this.wb_if.adr <= addr;
+        this.wb_if.we  <= 1'b0;
+        this.wb_if.sel <= mask;
+        this.wb_if.stb <= 1'b1;
+        this.wb_if.cyc <= 1'b1;
 
         do begin
-            @(posedge wb_if.clk);
+            @(posedge wb_if.clk_i);
         end while(this.wb_if.ack == 1'b0);
-
-        this.wb_if.stb = 1'b0;
-        this.wb_if.cyc = 1'b0;
-        read_data      = this.wb_if.dat_i;
-        @(posedge wb_if.clk);
+        
+        this.wb_if.stb <= 1'b0;
+        this.wb_if.cyc <= 1'b0;
+        read_data       = this.wb_if.dat_i;
+        @(posedge wb_if.clk_i);
         
     endtask
 
